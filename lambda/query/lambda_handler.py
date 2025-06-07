e533334fd5fd2eea3933742e644a2ed8eb95106e
Mo-Feng-33786267 <mfen0018@student.monash.edu>
2025-06-07 21:52:05 +1000

    Finalised API

--- lambda/query/lambda_handler.py
+++ lambda/query/lambda_handler.py
@@ -3,9 +3,23 @@ from handlers.search_by_species import handle as search_by_species
 from handlers.search_by_thumbnail import handle as search_by_thumbnail
 from handlers.manual_tag_edit import handle as manual_tag_edit
 from handlers.delete_files import handle as delete_files
+import json
 
 def lambda_handler(event, context):
+
+    try:
+        if isinstance(event.get("body"), str):
+            event["body"] = json.loads(event["body"])
+    except Exception as e:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": f"Invalid JSON body: {str(e)}"})
+        }
+
     action = event.get("action")
+    
+    if not action:
+        action = event["body"].get("action")
 
     if action == "search_by_tags":
         return search_by_tags(event)
